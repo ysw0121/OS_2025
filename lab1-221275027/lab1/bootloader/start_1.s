f566561a224b76c137917aab7325bcd308d21c25
ysw0121 <221275027@smail.nju.edu.cn>
2025-02-25 11:15:17 +0800

    lab1.0

---
+++ lab1-221275027/lab1/bootloader/start_1.s
@@ -0,0 +1,26 @@
+# TODO: This is lab1.1
+/* Real Mode Hello World */
+.code16
+
+.global start
+start:
+	movw %cs, %ax
+	movw %ax, %ds
+	movw %ax, %es
+	movw %ax, %ss
+	movw $0x7d00, %ax
+	movw %ax, %sp # setting stack pointer to 0x7d00
+	# TODO:通过中断输出Hello World, 并且间隔1000ms打印新的一行
+
+loop:
+	jmp loop
+
+message:
+	.string "Hello, World!\n\0"
+
+
+
+
+
+
+
