03d01ca44ab2f2d698586d9e522a85671ba00b19
ysw0121 <221275027@smail.nju.edu.cn>
2025-03-01 16:30:34 +0800

    exercise test for task lab

--- lab1-221275027/lab1/bootloader/start_1.s
+++ lab1-221275027/lab1/bootloader/start_1.s
@@ -12,11 +12,33 @@ start:
 	movw %ax, %sp # setting stack pointer to 0x7d00
 	# TODO:通过中断输出Hello World, 并且间隔1000ms打印新的一行
 
+	# output Hello World
+	movw $message, %bp
+	movw $13, %cx
+	movw $0x1301, %ax
+	movw $0x000c, %bx
+	movw $0x0000, %dx
+	int $0x10
+
+
 loop:
+	# 设置40ms定时器中断（约25次=1000ms）
+	movw $0x8600, %ax
+	movw $0x0028, %cx  # 40ms = 40000μs (0x9C40)
+	movw $0x9C40, %dx
+	int $0x15
+	
+	# 输出新行
+	movw $newline, %bp
+	movw $1, %cx
+	int $0x10
 	jmp loop
 
 message:
 	.string "Hello, World!\n\0"
+
+newline:
+	.string "\n\0"
 
 
 
