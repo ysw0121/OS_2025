73a7835c4e9a85d831654978f29ed0ac2719e7e6
ysw0121 <221275027@smail.nju.edu.cn>
2025-04-16 22:55:54 +0800

    temp failed

--- lab3-221275027/lab3/kernel/kernel/irqHandle.c
+++ lab3-221275027/lab3/kernel/kernel/irqHandle.c
@@ -5,6 +5,9 @@ extern TSS tss;
 extern ProcessTable pcb[MAX_PCB_NUM];
 extern int current;
 
+extern SegDesc gdt[NR_SEGMENTS];
+
+
 extern int displayRow;
 extern int displayCol;
 
@@ -66,21 +69,22 @@ uint32_t loadUMain(uint32_t first_sector, uint32_t sector_count, uint32_t pid)
 uint32_t schedule() {
 	// TODO: Select the next process and perform context switching
 
-	int i = (current + 1) % MAX_PCB_NUM;
-	while (i != current) {
-		if (i != 0 && pcb[i].state == STATE_RUNNABLE)
+	int if_find = 0;
+	for (int i = (current + 1) % MAX_PCB_NUM; i != current; i = (i+1) % MAX_PCB_NUM) {
+		if (pcb[i].state == STATE_RUNNABLE && i != 0) {
+			if_find = 1;
+			current = i;
 			break;
-		i = (i + 1) % MAX_PCB_NUM;
+		}
 	}
-	if (pcb[i].state != STATE_RUNNABLE)
-		i = 0;
-	current = i;
-    pcb[current].state = STATE_RUNNING;
-    uint32_t tmpStackTop = pcb[current].stackTop;
-    tss.esp0 = pcb[current].prevStackTop;
-    pcb[current].stackTop = pcb[current].prevStackTop;
-	asm volatile("movl %0, %%esp"::"m"(tmpStackTop));
-    asm volatile("popl %gs");
+	if (!if_find) current = 0;
+	pcb[current].state = STATE_RUNNING;
+
+	uint32_t tmpStackTop = pcb[current].stackTop;
+	pcb[current].stackTop = pcb[current].prevStackTop;
+	tss.esp0 = (uint32_t)&(pcb[current].stackTop);
+	asm volatile("movl %0, %%esp"::"m"(tmpStackTop)); // switch kernel stack
+	asm volatile("popl %gs");
 	asm volatile("popl %fs");
 	asm volatile("popl %es");
 	asm volatile("popl %ds");
@@ -88,8 +92,7 @@ uint32_t schedule() {
 	asm volatile("addl $8, %esp");
 	asm volatile("iret");
 
-
-	return -1;
+	return pcb[current].pid;
 }
 
 void irqHandle(struct StackFrame *sf)
@@ -131,21 +134,24 @@ void timerHandle(struct StackFrame *sf)
 {
 	// TODO
 
-	for (int i = 0; i < MAX_PCB_NUM; i++){
-		if (pcb[i].state == STATE_BLOCKED && pcb[i].sleepTime > 0){
-			pcb[i].sleepTime--;
-			if (pcb[i].sleepTime == 0){
-				pcb[i].state = STATE_RUNNABLE;
-			}
-		}
-		else
-			;
+	for (int i = 0; i < MAX_PCB_NUM; i++) {
+		if(pcb[i].state==STATE_BLOCKED){
+            pcb[i].sleepTime--;
+            if(pcb[i].sleepTime==0)pcb[i].state=STATE_RUNNABLE;
+        }
 	}
-	pcb[current].timeCount++;
-
-	if (pcb[current].state == STATE_BLOCKED || pcb[current].state == STATE_DEAD || pcb[current].timeCount >= MAX_TIME_COUNT || current == 0){
-		schedule();
-	}
+    if(pcb[current].state==STATE_RUNNING){
+        if(pcb[current].timeCount!=MAX_TIME_COUNT)pcb[current].timeCount++;
+        if(pcb[current].timeCount==MAX_TIME_COUNT){
+            pcb[current].timeCount=0;
+            pcb[current].state=STATE_RUNNABLE;
+        }
+    }
+    if(pcb[current].state!=STATE_RUNNING){
+        schedule();
+		
+    }
+    return ;
 }
 
 void syscallHandle(struct StackFrame *sf)
@@ -253,55 +259,66 @@ void sysFork(struct StackFrame *sf)
 	// 要做的是在寻找一个空闲的pcb做为子进程的进程控制块，
 	// 将父进程的资源复制给子进程。如果没有空闲pcb，则fork失败，父进程返回-1，成功则子进程返回0，父进程返回子进程pid
 
-
-	int pid = -1;
-	for (int i = 0; i < MAX_PCB_NUM; i++){
-		if (pcb[i].state == STATE_DEAD){
-            pid = i;
+	//寻找一个空闲的pcb
+	int index;
+	int i;
+	for(i=0;i<MAX_PCB_NUM;i++){
+		if(pcb[i].state==STATE_DEAD){
+			index=i;
 			break;
-        }
-	}
-	if (pid != -1){
-		for (int j = 0; j < 0x100000; j++) {
-			*(uint8_t *)(j + (pid+1)*0x100000) = *(uint8_t *)(j + (current+1)*0x100000);
 		}
-		pcb[pid].pid=pid;
-		pcb[pid].prevStackTop=pcb[current].prevStackTop+(uint32_t)&pcb[pid]-(uint32_t)&pcb[current];
-        pcb[pid].stackTop=pcb[current].stackTop + (uint32_t)&pcb[pid]-(uint32_t)&pcb[current];
-		pcb[pid].sleepTime=0;		
-		pcb[pid].state = STATE_RUNNABLE;
-		pcb[pid].timeCount = 0;
-		pcb[pid].regs.edi = sf->edi;
-		pcb[pid].regs.esi = sf->esi;
-		pcb[pid].regs.ebp = sf->ebp;
-		pcb[pid].regs.xxx = sf->xxx;
-		pcb[pid].regs.ebx = sf->ebx;
-		pcb[pid].regs.edx = sf->edx;
-		pcb[pid].regs.ecx = sf->ecx;
-		pcb[pid].regs.eax = sf->eax;
-		pcb[pid].regs.irq = sf->irq;
-		pcb[pid].regs.error = sf->error;
-		pcb[pid].regs.eip = sf->eip;
-		pcb[pid].regs.esp = sf->esp;
-		pcb[pid].regs.eflags = sf->eflags;
-		pcb[pid].regs.cs = USEL((1 + pid * 2));
-		pcb[pid].regs.ss = USEL((2 + pid * 2));
-		pcb[pid].regs.ds = USEL((2 + pid * 2));
-		pcb[pid].regs.es = USEL((2 + pid * 2));
-		pcb[pid].regs.fs = USEL((2 + pid * 2));
-		pcb[pid].regs.gs = USEL((2 + pid * 2));
-		pcb[pid].regs.eax = 0; 
-		pcb[current].regs.eax = pid; 
 	}
-	else pcb[current].regs.eax = -1; 
-	return;
+	if(i==MAX_PCB_NUM){
+		pcb[current].regs.eax=-1;
+		return;
+	}
 
+	//复制资源并初始化
+	enableInterrupt();
+	for (int j = 0; j < 0x100000; j++) {
+		*(unsigned char *)(j + (index + 1) * 0x100000) = *(unsigned char *)(j + (current + 1) * 0x100000);
+		asm volatile("int $0x20");
+	}
+	disableInterrupt();
+
+	pcb[index].pid = index;
+	pcb[index].prevStackTop = pcb[current].prevStackTop - (uint32_t)&(pcb[current]) + (uint32_t)&(pcb[index]);
+	pcb[index].sleepTime = 0;
+
+	pcb[index].stackTop = pcb[current].stackTop - (uint32_t)&(pcb[current]) + (uint32_t)&(pcb[index]);
+
+	pcb[index].state = STATE_RUNNABLE;
+	pcb[index].timeCount = 0;
+
+	pcb[index].regs.edi = pcb[current].regs.edi;
+	pcb[index].regs.esi = pcb[current].regs.esi;
+	pcb[index].regs.ebp = pcb[current].regs.ebp;
+	pcb[index].regs.xxx = pcb[current].regs.xxx;
+	pcb[index].regs.ebx = pcb[current].regs.ebx;
+	pcb[index].regs.edx = pcb[current].regs.edx;
+	pcb[index].regs.ecx = pcb[current].regs.ecx;
+	pcb[index].regs.eax = pcb[current].regs.eax;
+	pcb[index].regs.irq = pcb[current].regs.irq;
+	pcb[index].regs.error = pcb[current].regs.error;
+	pcb[index].regs.eip = pcb[current].regs.eip;
+	pcb[index].regs.eflags = pcb[current].regs.eflags;
+	pcb[index].regs.esp = pcb[current].regs.esp;
+
+	pcb[index].regs.cs = USEL(1 + 2*index);
+	pcb[index].regs.ss = USEL(2 * (index+1));
+	pcb[index].regs.ds = USEL(2 * (index+1));
+	pcb[index].regs.es = USEL(2 * (index+1));
+	pcb[index].regs.fs = USEL(2 * (index+1));
+	pcb[index].regs.gs = USEL(2 * (index+1));
+
+	pcb[current].regs.eax = index;
+	pcb[index].regs.eax = 0;
+	return;
+	
 
 }
 
-void sysExec(struct StackFrame *sf)
-{
-	// TODO: finish exec
+void sysExec(struct StackFrame *sf) {
 	// sysExec是操作系统内核中实现程序替换功能的关键系统调用，它负责将当前进程的内存映像替换为新的程序映像。
 
 	//	sysExec主要完成以下任务：
@@ -310,7 +327,7 @@ void sysExec(struct StackFrame *sf)
 	//	3. 开始执行新程序
 
 	
-
+	return;
 }
 
 void sysSleep(struct StackFrame *sf)
@@ -318,10 +335,20 @@ void sysSleep(struct StackFrame *sf)
 	// TODO: finish sleep
 
 	// 将当前的进程的sleepTime设置为传入的参数，将当前进程的状态设置为STATE_BLOCKED
-	pcb[current].sleepTime = sf->ebx;
+
+	if(sf->ecx<0){
+		pcb[current].regs.eax = -1;
+		return;
+	}
 	pcb[current].state = STATE_BLOCKED;
-	asm("int $0x20");
+	pcb[current].sleepTime = sf->ecx;
+
+	schedule();
+	
+	pcb[current].regs.eax = 0;
 	return;
+
+	
 
 }
 
@@ -329,9 +356,19 @@ void sysExit(struct StackFrame *sf)
 {
 	// TODO: finish exit
 	// 将当前进程的状态设置为STATE_DEAD，然后模拟时钟中断进行进程切换
+
+	// pcb[current].state = STATE_DEAD;
+	// pcb[current].pid=schedule();
+	// // asm volatile("int $0x20");
+	// sf->eax = 0;
+	// return;
+
 	pcb[current].state = STATE_DEAD;
-	asm("int $0x20");
+	pcb[current].timeCount = MAX_TIME_COUNT;
+	asm volatile("int $0x20");
 	return;
+
+	
 }
 
 void sysGetPid(struct StackFrame *sf)
