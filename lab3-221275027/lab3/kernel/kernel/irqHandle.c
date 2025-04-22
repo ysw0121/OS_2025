25d646a2b147b55c6a441b9f41490400df9b63a3
ysw0121 <221275027@smail.nju.edu.cn>
2025-04-22 16:18:37 +0800

    exec test

--- lab3-221275027/lab3/kernel/kernel/irqHandle.c
+++ lab3-221275027/lab3/kernel/kernel/irqHandle.c
@@ -80,26 +80,28 @@ uint32_t schedule()
 		current = (current + 1) % MAX_PCB_NUM;
 		if (pcb[current].state == STATE_RUNNABLE)
 		{
-			pcb[current].state = STATE_RUNNING;
+			// pcb[current].state = STATE_RUNNING;
 			pcb[current].timeCount = 0;
-			uint32_t tmpStackTop = pcb[current].stackTop;
-			pcb[current].stackTop = pcb[current].prevStackTop;
-			tss.esp0 = (uint32_t) & (pcb[current].stackTop);
-			asm volatile("movl %0, %%esp" ::"m"(tmpStackTop)); // switch kernel stack
-			asm volatile("popl %gs");
-			asm volatile("popl %fs");
-			asm volatile("popl %es");
-			asm volatile("popl %ds");
-			asm volatile("popal");
-			asm volatile("addl $8, %esp");
-			asm volatile("iret");
-			return current; // 返回新调度进程的PID
+			// uint32_t tmpStackTop = pcb[current].stackTop;
+			// pcb[current].stackTop = pcb[current].prevStackTop;
+			// tss.esp0 = (uint32_t) & (pcb[current].stackTop);
+			// asm volatile("movl %0, %%esp" ::"m"(tmpStackTop)); // switch kernel stack
+			// asm volatile("popl %gs");
+			// asm volatile("popl %fs");
+			// asm volatile("popl %es");
+			// asm volatile("popl %ds");
+			// asm volatile("popal");
+			// asm volatile("addl $8, %esp");
+			// asm volatile("iret");
+			contextSwitch(current, current);
+			return pcb[current].pid; // 返回新调度进程的PID
 		}
 	}
 	
 	// 2. 如果没有可运行的用户进程，检查当前进程状态
 	if (pcb[current].state == STATE_RUNNING)
 	{
+		return -1;
 		pcb[current].state = STATE_RUNNABLE;
 		pcb[current].timeCount = 0;
 	}
@@ -107,18 +109,19 @@ uint32_t schedule()
 	// 3. 回退到内核进程(pid=0)
 	current = 0;
 	pcb[current].timeCount = 0;
-	uint32_t tmpStackTop = pcb[current].stackTop;
-	pcb[current].stackTop = pcb[current].prevStackTop;
-	tss.esp0 = (uint32_t) & (pcb[current].stackTop);
-	asm volatile("movl %0, %%esp" ::"m"(tmpStackTop)); // switch kernel stack
-	asm volatile("popl %gs");
-	asm volatile("popl %fs");
-	asm volatile("popl %es");
-	asm volatile("popl %ds");
-	asm volatile("popal");
-	asm volatile("addl $8, %esp");
-	asm volatile("iret");
-	return -1; // 返回内核进程PID
+	// uint32_t tmpStackTop = pcb[current].stackTop;
+	// pcb[current].stackTop = pcb[current].prevStackTop;
+	// tss.esp0 = (uint32_t) & (pcb[current].stackTop);
+	// asm volatile("movl %0, %%esp" ::"m"(tmpStackTop)); // switch kernel stack
+	// asm volatile("popl %gs");
+	// asm volatile("popl %fs");
+	// asm volatile("popl %es");
+	// asm volatile("popl %ds");
+	// asm volatile("popal");
+	// asm volatile("addl $8, %esp");
+	// asm volatile("iret");
+	contextSwitch(current, current);
+	return pcb[current].pid; // 返回内核进程PID
 }
 
 void contextSwitch(int prev, int new)
@@ -182,24 +185,48 @@ void timerHandle(struct StackFrame *sf)
 {
 	// TODO
 
-	for (int i = 0; i < MAX_PCB_NUM; i++)
-	{
-		if (pcb[i].state == STATE_BLOCKED && pcb[i].sleepTime > 0)
-		{
-			pcb[i].sleepTime--;
-			if (pcb[i].sleepTime == 0)
-			{
-				pcb[i].state = STATE_RUNNABLE;
-			}
+	int i;
+	for (i = 0; i < MAX_PCB_NUM; i++) {
+		if (pcb[i].state == STATE_BLOCKED) {
+		    pcb[i].sleepTime--;
+		    if(pcb[i].sleepTime == 0)
+		    	pcb[i].state = STATE_RUNNABLE;
 		}
-		else
-			;
 	}
-	pcb[current].timeCount++;
-	if (pcb[current].state == STATE_BLOCKED || pcb[current].state == STATE_DEAD || pcb[current].timeCount >= MAX_TIME_COUNT || current == 0)
-	{
-		schedule();
+	if (pcb[current].state == STATE_RUNNING) {
+		if (pcb[current].timeCount < MAX_TIME_COUNT)
+			pcb[current].timeCount++;
+		else {
+	    		pcb[current].timeCount = 0;
+	    		pcb[current].state = STATE_RUNNABLE;
+		}
 	}
+	if (pcb[current].state != STATE_RUNNING) {
+		for (i = 0; i < MAX_PCB_NUM; i++){
+			if (i != current && pcb[i].state == STATE_RUNNABLE) break;
+		}
+		if (i == MAX_PCB_NUM) i = 0;
+		
+		current = i;	
+		pcb[current].state = STATE_RUNNING;
+		
+
+		contextSwitch(current, current);
+		// schedule();
+		// uint32_t tmpStackTop = pcb[current].stackTop;
+		// // tss.esp0 = pcb[current].prevStackTop;
+		// tss.esp0 = (uint32_t) & (pcb[current].stackTop);
+		// pcb[current].stackTop = pcb[current].prevStackTop;
+		// asm volatile("movl %0, %%esp"::"m"(tmpStackTop));
+		// asm volatile("popl %gs");
+		// asm volatile("popl %fs");
+		// asm volatile("popl %es");
+		// asm volatile("popl %ds");
+		// asm volatile("popal");
+		// asm volatile("addl $8, %esp");
+		// asm volatile("iret");
+	}
+	return;
 }
 
 void syscallHandle(struct StackFrame *sf)
@@ -306,52 +333,51 @@ void sysFork(struct StackFrame *sf)
 	// 要做的是在寻找一个空闲的pcb做为子进程的进程控制块，
 	// 将父进程的资源复制给子进程。如果没有空闲pcb，则fork失败，父进程返回-1，成功则子进程返回0，父进程返回子进程pid
 
-	int newpid = -1;
-	for (int i = 0; i < MAX_PCB_NUM; i++)
-	{
-		if (pcb[i].state == STATE_DEAD)
-		{
-			newpid = i;
-			break;
-		}
+	int new_index = 0;
+	while(new_index < MAX_PCB_NUM && pcb[new_index].state != STATE_DEAD){
+		new_index++;
 	}
-	if (newpid == -1)
-	{
+	if(new_index == MAX_PCB_NUM){
 		pcb[current].regs.eax = -1;
 		return;
 	}
 
-	// 复制内存
-	enableInterrupt();
-	for (int j = 0; j < 0x100000; j++)
-	{
-		*(uint8_t *)(j + (newpid + 1) * 0x100000) = *(uint8_t *)(j + (current + 1) * 0x100000);
-	}
-	disableInterrupt();
-	// 基本复制
-	// pcb[newpid] = pcb[current]; what the fuck???
-	for (int i = 0; i < sizeof(ProcessTable); i++)
-	{
-		*((uint8_t *)(&pcb[newpid]) + i) = *((uint8_t *)(&pcb[current]) + i);
+	for(int i = 0; i < 0x100000; i++){
+		*(unsigned char *)(i + (new_index + 1) * 0x100000) = *(unsigned char *)(i + (current + 1) * 0x100000);
 	}
 
-	pcb[newpid].state = STATE_RUNNABLE;
-	pcb[newpid].timeCount = 0;
-	pcb[newpid].sleepTime = 0;
-	pcb[newpid].pid = newpid;
-	pcb[newpid].stackTop = (uint32_t) & (pcb[newpid].regs);
-	pcb[newpid].prevStackTop = (uint32_t) & (pcb[newpid].stackTop);
-	// 返回值
-	pcb[newpid].regs.eax = 0;
-	pcb[current].regs.eax = newpid;
+	pcb[new_index].pid = new_index;
+	pcb[new_index].state = STATE_RUNNABLE;
+	pcb[new_index].timeCount = 0;
+	pcb[new_index].sleepTime = 0;
 
-	// 段表，寄存器
-	gdt[1 + newpid * 2] = SEG(STA_X | STA_R, (newpid + 1) * 0x100000, 0x00100000, DPL_USER);
-	gdt[2 + newpid * 2] = SEG(STA_W, (newpid + 1) * 0x100000, 0x00100000, DPL_USER);
-	setGdt(gdt, sizeof(gdt));
-	pcb[newpid].regs.cs = USEL(1 + 2 * newpid);
-	pcb[newpid].regs.ds = pcb[newpid].regs.es = pcb[newpid].regs.fs = pcb[newpid].regs.gs = pcb[newpid].regs.ss = USEL(2 + 2 * newpid);
-	
+	pcb[new_index].stackTop = pcb[current].stackTop - (uint32_t)&(pcb[current]) + (uint32_t)&(pcb[new_index]);
+	pcb[new_index].prevStackTop = pcb[current].prevStackTop - (uint32_t)&(pcb[current]) + (uint32_t)&(pcb[new_index]);
+
+	pcb[new_index].regs.edi = pcb[current].regs.edi;
+	pcb[new_index].regs.esi = pcb[current].regs.esi;
+	pcb[new_index].regs.ebp = pcb[current].regs.ebp;
+	pcb[new_index].regs.xxx = pcb[current].regs.xxx;
+	pcb[new_index].regs.ebx = pcb[current].regs.ebx;
+	pcb[new_index].regs.edx = pcb[current].regs.edx;
+	pcb[new_index].regs.ecx = pcb[current].regs.ecx;
+	pcb[new_index].regs.eax = pcb[current].regs.eax;
+	pcb[new_index].regs.irq = pcb[current].regs.irq;
+	pcb[new_index].regs.error = pcb[current].regs.error;
+	pcb[new_index].regs.eip = pcb[current].regs.eip;
+	pcb[new_index].regs.eflags = pcb[current].regs.eflags;
+	pcb[new_index].regs.esp = pcb[current].regs.esp;
+
+	pcb[new_index].regs.cs = USEL(2*new_index + 1);
+	pcb[new_index].regs.ss = USEL(2*new_index + 2);
+	pcb[new_index].regs.ds = USEL(2*new_index + 2);
+	pcb[new_index].regs.es = USEL(2*new_index + 2);
+	pcb[new_index].regs.fs = USEL(2*new_index + 2);
+	pcb[new_index].regs.gs = USEL(2*new_index + 2);
+
+	pcb[current].regs.eax = new_index;
+	pcb[new_index].regs.eax = 0;
+	return;
 
 }
 
@@ -364,8 +390,42 @@ void sysExec(struct StackFrame *sf)
 	//	2. 初始化新程序的执行环境
 	//	3. 开始执行新程序
 
-	
-	return;
+	uint32_t entry = loadUMain(sf->ecx, sf->edx, current);
+
+    // 2. 初始化新程序的执行环境
+    sf->eip = entry;  // 设置程序入口地址
+    sf->esp = 0x100000;  // 设置用户栈指针
+	sf->ebp = 0x100000;  // 设置基址指针
+	sf->irq = 0;  // 清除中断号
+	sf->error = 0;  // 清除错误码
+
+    
+    // 设置段寄存器为用户态
+    sf->cs = USEL((2 * current + 1));  // 代码段
+    sf->ds = USEL((2 * current + 2));  // 数据段
+    sf->es = USEL((2 * current + 2));
+    sf->fs = USEL((2 * current + 2));
+    sf->gs = USEL((2 * current + 2));
+    sf->ss = USEL((2 * current + 2));
+    
+    // 清空寄存器状态
+    sf->eax = 0;
+    sf->ebx = 0;
+    sf->ecx = 0;
+    sf->edx = 0;
+    sf->edi = 0;
+    sf->esi = 0;
+    sf->ebp = 0;
+    
+    // 设置EFLAGS
+    sf->eflags = 0x202;  // 启用中断+用户态
+
+    // 3. 开始执行新程序
+    // 通过设置好的寄存器状态，在返回时会自动开始执行新程序
+	contextSwitch(current, current);
+
+    return;
+
 }
 
 void sysSleep(struct StackFrame *sf)
@@ -373,12 +433,17 @@ void sysSleep(struct StackFrame *sf)
 	// TODO: finish sleep
 
 	// 将当前的进程的sleepTime设置为传入的参数，将当前进程的状态设置为STATE_BLOCKED
+	if(sf->ecx > 0){
+		pcb[current].state = STATE_BLOCKED;
+		pcb[current].sleepTime = sf->ecx;
+		asm volatile("int $0x20");
 
-	pcb[current].state = STATE_BLOCKED;
-	assert(sf->ecx > 0);
-	pcb[current].sleepTime = sf->ecx;
-	asm volatile("int $0x20");
-	return;
+		contextSwitch(current, current);
+
+
+		return;
+	}
+	
 
 	
 
@@ -391,6 +456,9 @@ void sysExit(struct StackFrame *sf)
 
 	pcb[current].state = STATE_DEAD;
 	asm volatile("int $0x20");
+
+	contextSwitch(current, current);
+
 	return;
 
 	
